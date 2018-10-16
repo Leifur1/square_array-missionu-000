8bd8aa3f87ed7b95b4811f3a0c883914ecd4d5af
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-16 22:18:23 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   new_numbers = []
-  array.each do |square|
-    new_numbers << square ** 2
+  array.collect do |i| i ** 2
+  # array.each do |square|
+  #   new_numbers << square ** 2
   end
   new_numbers
 end
